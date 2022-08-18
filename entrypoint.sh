7f3bf39c90eaae195037cd3404251e2a61697ae5
Jose Palacios <jmpd1988@gmail.com>
2022-08-18 12:27:02 -0500

    fixit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 
 BUILD_ARGS=""
-if [ -z "$INPUT_BUILD_ARGS" ]
+if [ ! -z "${INPUT_BUILD_ARGS}" ]
 then
-    BUILD_ARGS="--build-arg ${$INPUT_BUILD_ARGS}"
+    BUILD_ARGS="--build-arg ${INPUT_BUILD_ARGS}"
 fi
 
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
