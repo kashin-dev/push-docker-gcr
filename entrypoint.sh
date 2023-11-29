a7647f5d46e93a2598237749704b6ade91ff5a96
Daniel Mateos <dmateosc@outlook.es>
2023-11-29 19:29:26 +0100

    feat: adding build_params input

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,14 @@ then
     BUILD_ARGS="--build-arg ${INPUT_BUILD_ARGS}"
 fi
 
+BUILD_PARAMS=""
+if [ ! -z "${INPUT_BUILD_PARAMS}" ]
+then
+    BUILD_PARAMS=$INPUT_BUILD_PARAMS
+fi
+
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build $BUILD_ARGS -t $IMAGE_NAME -f $INPUT_FILE .
+
+docker build $BUILD_ARGS $BUILD_PARAMS -t $IMAGE_NAME -f $INPUT_FILE .
+
 docker push $IMAGE_NAME 
